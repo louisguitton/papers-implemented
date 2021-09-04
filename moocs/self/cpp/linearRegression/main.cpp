ff1c432b068b5262ffb3f3b8998aa218d4197153
Louis Guitton <louisguitton93@gmail.com>
2021-09-04 19:30:14 +0200

    use vcpkg

--- moocs/self/cpp/linearRegression/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "utilities.h"
-
-
-int main()
-{
-    int c = sum(2, 3);
-    std::cout << c << std::endl;
-}
---
+++ moocs/self/cpp/linearRegression/src/main.cpp
@@ -0,0 +1,8 @@
+#include <sqlite3.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\n", sqlite3_libversion());
+    return 0;
+}
