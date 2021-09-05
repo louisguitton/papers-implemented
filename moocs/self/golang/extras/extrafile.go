11a8da72b1aefb7f5b5ffc834be925533f310437
Louis Guitton <louisguitton93@gmail.com>
2021-09-05 12:49:26 +0200

    add more golang

--- moocs/self/golang/extras/extrafile.go
+++ moocs/self/golang/extras/extrafile.go
@@ -1,6 +1,6 @@
 package extras
 
-
+/*Yo from the docs*/
 func HelloFromExtras() string {
 	return "Hello From Extras!"
 }
--- moocs/self/golang/main.go
+++ moocs/self/golang/main.go
@@ -5,8 +5,22 @@ import (
 	extras "github.com/louisguitton/papers-implemented/moocs/self/golang/extras"
 )
 
+type Employee struct {
+	name string
+	age  int
+}
 
 func main() {
 	fmt.Println("Hello world")
 	fmt.Println(extras.HelloFromExtras())
+
+	myseasons := [...]string{"Printemps", "été", "Automne", "Hiver"}
+	fmt.Println(myseasons, len(myseasons))
+
+	for i, s := range myseasons {
+		fmt.Println(i, s)
+	}
+
+	e := Employee{name: "Louis"}
+	e.age = 27
 }
