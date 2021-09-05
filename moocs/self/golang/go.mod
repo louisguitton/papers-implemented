7c023ad9bf515c8bd51af2cc888123d93f3d48dd
Louis Guitton <louisguitton93@gmail.com>
2021-09-05 13:58:03 +0200

    add golearn hello world

--- moocs/self/golang/go.mod
+++ moocs/self/golang/go.mod
@@ -3,10 +3,23 @@ module github.com/louisguitton/papers-implemented/moocs/self/golang
 go 1.17
 
 require (
+	github.com/go-gota/gota v0.11.0 // indirect
+	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
+	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
+	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
+	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/guptarohit/asciigraph v0.5.1 // indirect
+	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
+	github.com/rocketlaunchr/dataframe-go v0.0.0-20201007021539-67b046771f0b // indirect
+	github.com/sjwhitworth/golearn v0.0.0-20210511113939-00d4cfd91a1b // indirect
 	github.com/yuin/goldmark v1.3.5 // indirect
+	golang.org/x/exp v0.0.0-20200331195152-e8c3332aa8e5 // indirect
 	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/tools v0.1.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gonum.org/v1/gonum v0.9.3 // indirect
 )
--- moocs/self/golang/main.go
+++ moocs/self/golang/main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
 	extras "github.com/louisguitton/papers-implemented/moocs/self/golang/extras"
+
+	"github.com/go-gota/gota/dataframe"
+
+	"github.com/sjwhitworth/golearn/base"
+	"github.com/sjwhitworth/golearn/evaluation"
+	"github.com/sjwhitworth/golearn/knn"
 )
 
 type Employee struct {
@@ -23,4 +32,42 @@ func main() {
 
 	e := Employee{name: "Louis"}
 	e.age = 27
+
+	// Load dataset with a dataframe
+	irisCsv, err := os.Open("Iris.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	df := dataframe.ReadCSV(irisCsv)
+	fmt.Println(df)
+
+	// Load dataset in golearn
+	rawData, err := base.ParseCSVToInstances("Iris.csv", true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(rawData)
+
+	// Initialises a new KNN classifier
+	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
+
+	// Do a training-test split
+	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.8)
+	cls.Fit(trainData)
+
+	// Calculates the Euclidean distance and returns the most popular label
+	predictions, err := cls.Predict(testData)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(predictions)
+
+	// Prints precision/recall metrics
+	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
+	}
+	fmt.Println(evaluation.GetSummary(confusionMat))
 }
